3af57e8271deeda981b0ba4c2d1c319943d6758f
ABHI GUPTA <138576028+coder3005@users.noreply.github.com>
2023-07-05 11:07:59 +0530

    Create abstract.java

---
+++ abstract.java
@@ -0,0 +1,15 @@
+/* abstract class */
+abstract class  animal
+{
+
+}
+class dog extends animal 
+{
+
+}
+class demo
+{
+    public static void main(String[] args) {
+        animal r=new dog();
+    }
+}
